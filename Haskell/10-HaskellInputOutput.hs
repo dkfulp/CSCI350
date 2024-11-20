dfffe70d21115d62dbbae982733704d28889a3f4
dkfulp <dakotafulp@gmail.com>
2024-11-20 11:55:10 -0500

    Adding last haskell code

--- Haskell/10-HaskellInputOutput.hs
+++ Haskell/10-HaskellInputOutput.hs
@@ -1,73 +1,44 @@
 -- Working with Higher Order Functions in Haskell
 -- Example program that creates a basic Haskell program that demonstrates:
--- 1. Curried Functions
--- 2. Higher-Orderisms in Order
--- 3. Maps and Filters
--- 4. Lambdas 
--- 5. Folds and Scans
--- 6. Function Application with $
--- 7. Function Composition
+-- 1. Inputting user input
+-- 2. Checking it against different cases
+-- 3. Performing actions and using recursion to continue the loop
 
+-- Function to perform a computation based on the user's choice
+performComputation :: Int -> Int -> Int
+performComputation 1 n = n * 2  -- Double
+performComputation 2 n = n * n  -- Square
+performComputation 3 n = n + 1  -- Increment
+performComputation _ n = n      -- Default case: return the number unchanged
 
+-- Main recursive loop
+mainLoop :: IO ()
+mainLoop = do
+    putStrLn "Enter a number:"
+    numberInput <- getLine
+    let maybeNumber = reads numberInput :: [(Int, String)]
+    case maybeNumber of
+        [(number, "")] -> do
+            putStrLn "Choose an operation by entering the corresponding number:"
+            putStrLn "1. Double the number"
+            putStrLn "2. Square the number"
+            putStrLn "3. Increment the number by 1"
+            operationInput <- getLine
+            let maybeOperation = reads operationInput :: [(Int, String)]
+            case maybeOperation of
+                [(operation, "")] | operation `elem` [1, 2, 3] -> do
+                    let result = performComputation operation number
+                    putStrLn $ "Result: " ++ show result
+                    putStrLn "Do you want to perform another computation? (yes/no):"
+                    continue <- getLine
+                    if continue == "yes"
+                        then mainLoop
+                        else putStrLn "Goodbye!"
+                _ -> putStrLn "Invalid operation. Please enter 1, 2, or 3." >> mainLoop
+        _ -> putStrLn "Invalid input. Please enter a valid number." >> mainLoop
 
--- PART 1 ****************************************
--- Every function only takes one parameter, so how have they
--- been taking multiple then? It's a clever trick called currying 
-
--- The following two are the same thing
--- ghci> max 4 5
--- ghci> (max 4) 5
-
--- The space between two things is function application.
--- The space acts like an operator and has the highest precedence
-
--- Looking at max we see max :: (Ord a) => a -> a -> a
--- This can also be written as max :: (Ord a) => a -> (a -> a)
--- This reads as max takes an a and returns a function that 
--- takes an a and returns an a.
-
-
--- Let's look at this function
-multThree :: (Num a) => a -> a -> a -> a
-multThree x y z = x * y * z
-
--- What really happens when given multThree 3 5 9?
--- 1st, multThree y z = 3 * y * z
--- 2nd, multThree z = 3 * 5 * z
--- Finally, 9 is appiled to the function to get 135
-
-
--- We can also partially use a function to create new 
--- functions on the fly 
--- ghci> let multTwoWithNine = multThree 9
--- ghci> multTwoWithNine 2 3
--- Output: 54
-
--- ghci> let multWithEighteen = multTwoWithNine 2
--- ghci> multWithEighteen 10
--- Output: 180
-
-
-
--- PART 2 ****************************************
-
-
--- PART 3 ****************************************
-
-
--- PART 4 ****************************************
-
-
-
+-- Entry point
 main :: IO ()
 main = do
-    -- PART 1 ****************************************
-   
-
-    -- PART 2 ****************************************
-
-
-    -- PART 3 ****************************************
-
-
-    -- PART 4 ****************************************
+    putStrLn "Welcome to the number computation program!"
+    mainLoop
